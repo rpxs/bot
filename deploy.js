b8d0fa0788c63749609d242e91d83d0c8d360337
blurhash <82728087+blurhash@users.noreply.github.com>
2021-09-06 17:18:04 -0600

    Create deploy.js

---
+++ deploy.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+require("dotenv").config();
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const { CLIENT_ID, GUILD_ID, TOKEN } = process.env;
+const commands = [];
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
+}
+const rest = new REST({ version: "9" }).setToken(TOKEN);
+
+(async () => {
+  try {
+    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+      body: commands,
+    });
+
+    console.log("Successfully registered application commands.");
+  } catch (error) {
+    console.error(error);
+  }
+})();
