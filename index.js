f713a6a9005f7f7ccdd7fd1d0f4a2d69af0a1ac9
blurhash <82728087+blurhash@users.noreply.github.com>
2021-09-06 17:18:20 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+const { Client, Intents, Collection } = require("discord.js");
+const fs = require("fs");
+require("dotenv").config();
+const { TOKEN } = process.env;
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+client.commands = new Collection();
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
+}
+// Create a new client instance
+// When the client is ready, run this code (only once)
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    return interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
+});
+
+// Login to Discord with your client's token
+client.login(TOKEN);
